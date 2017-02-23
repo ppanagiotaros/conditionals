972063ad38360b56e976b05d7beac20778be007b
ppanagiotaros <ppanagiotaros@sjnma.org>
2017-02-23 10:57:21 -0600

    problem 1 done

---
+++ Problem1.py
@@ -0,0 +1,12 @@
+firstnumber = int(raw_input("first number"))
+secondnumber = int(raw_input("second number"))
+thirdnumber = int(raw_input("third number"))
+
+if( firstnumber + secondnumber == thirdnumber):
+	print("true")
+
+if( secondnumber + thirdnumber == firstnumber):
+	print("true")
+
+if( thirdnumber + firstnumber == secondnumber):
+	print("true")	
